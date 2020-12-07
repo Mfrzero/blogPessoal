534cc50c91c765c65fceb3a9542e56e6424c8345
Matheus <you@example.com>
2020-12-07 10:22:12 -0300

    criação do cadastro de usuario

--- blogPessoal/src/app/login/login.component.ts
+++ blogPessoal/src/app/login/login.component.ts
@@ -1,3 +1,6 @@
+import { UserLogin } from './../model/UserLogin';
+import { Router } from '@angular/router';
+import { AuthService } from './../service/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userLogin: UserLogin = new UserLogin();
+  constructor(
+    private authService: AuthService,
+    private router : Router
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  entrar(){
+    this.authService.logar(this.userLogin).subscribe((resp: UserLogin) =>{
+      this.userLogin = resp
+      localStorage.setItem('token',this.userLogin.token)
+      this.router.navigate(['/feed'])
+    })
+  }
 }
--- blogPessoal/src/app/navbar/navbar.component.ts
+++ blogPessoal/src/app/navbar/navbar.component.ts
@@ -12,4 +12,8 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  sair(){
+    localStorage.clear()
+  }
+
 }
